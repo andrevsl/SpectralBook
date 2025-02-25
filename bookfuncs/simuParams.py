7b0c89d7c4691f822dad686207086c3b54bdb4d5
andrevsl <andrevsl@gmail.com>
2025-02-25 13:48:43 -0300

    Antenna Chap

--- bookfuncs/simuParams.py
+++ bookfuncs/simuParams.py
@@ -7,17 +7,17 @@ import numpy as np
 class SimuParam():
 
     def __init__(self):
-        self.emconst=pd.DataFrame( {"e0": [8.854223e-12],   #Permissividade absoluta do meio Free space
+        self.emconst= {"e0": 8.854223e-12,   #Permissividade absoluta do meio Free space
                         "er": [2.2],   #Permissividade relativa do meio, primeira casa meio 1
                         "ereff": [2.2],   #Permissividade efetiva do meio 
-                        "u0": [1.256640e-6],      #Permeabilidade absoluta do meio Free space
+                        "u0": 1.256640e-6,      #Permeabilidade absoluta do meio Free space
                         "ur": [1],   # Permeabilidade relativa do meio 1, primeira casa meio 1
-                        "eta0": [np.sqrt(1.256640e-6/8.854223e-12)],   # Impedancia de Onda Free space
+                        "eta0": np.sqrt(1.256640e-6/8.854223e-12),   # Impedancia de Onda Free space
                         "sig": [0],   # Condutividade do meio 1, primeira casa meio 1
                         "cc": 1/np.sqrt(1.256640e-6*8.854223e-12),   # Light Velocity
                         
                       } 
-            )
+            
         self.par=pd.DataFrame( {"Name": ['HSinP'], 
                                     "t0": [0], # time stamp start
                                     "tf": [0], # time stamp end
@@ -58,7 +58,7 @@ class SimuParam():
         self.par.loc['HSinP','tf']=5e-9 # Sampling Frequency
         self.par.loc['HSinP','Nt']=int(len(np.arange(self.par.loc['HSinP','t0'],self.par.loc['HSinP','tf'],self.par.loc['HSinP','dt']))) # x space Points (odd)
 
-        self.par.loc['HSinP','Lamb']=1/np.sqrt(self.emconst['e0'][0]*self.emconst['u0'][0])/self.par.loc['HSinP','f']# Frequency
+        self.par.loc['HSinP','Lamb']=1/np.sqrt(self.emconst['e0']*self.emconst['u0'])/self.par.loc['HSinP','f']# Frequency
         self.par.loc['HSinP','kx']=2*np.pi/self.par.loc['HSinP','Lamb'] # Frequency
         self.par.loc['HSinP','dx']= self.par.loc['HSinP','Lamb']/20 # x Delta
         self.par.loc['HSinP','Fx']= 1/self.par.loc['HSinP','dx']
@@ -76,7 +76,7 @@ class SimuParam():
         self.par.loc['HSinN','tf']=5e-9 # Sampling Frequency
         self.par.loc['HSinN','Nt']=int(len(np.arange(self.par.loc['HSinN','t0'],self.par.loc['HSinN','tf'],self.par.loc['HSinN','dt']))) # x space Points (odd)
 
-        self.par.loc['HSinN','Lamb']=1/np.sqrt(self.emconst['e0'][0]*self.emconst['u0'][0])/self.par.loc['HSinN','f']# Frequency
+        self.par.loc['HSinN','Lamb']=1/np.sqrt(self.emconst['e0']*self.emconst['u0'])/self.par.loc['HSinN','f']# Frequency
         self.par.loc['HSinN','kx']=2*np.pi/self.par.loc['HSinN','Lamb'] # Frequency
         self.par.loc['HSinN','dx']= self.par.loc['HSinN','Lamb']/20 # x Delta
         self.par.loc['HSinN','Fx']= 1/self.par.loc['HSinN','dx']
@@ -94,7 +94,7 @@ class SimuParam():
         self.par.loc['Gau','tf']=5e-9 # Sampling Frequency
         self.par.loc['Gau','Nt']=int(len(np.arange(self.par.loc['Gau','t0'],self.par.loc['Gau','tf'],self.par.loc['Gau','dt']))) # x space Points (odd)
 
-        self.par.loc['Gau','Lamb']=1/np.sqrt(self.emconst['e0'][0]*self.emconst['u0'][0])/self.par.loc['Gau','f']# Frequency
+        self.par.loc['Gau','Lamb']=1/np.sqrt(self.emconst['e0']*self.emconst['u0'])/self.par.loc['Gau','f']# Frequency
         self.par.loc['Gau','kx']=2*np.pi/self.par.loc['Gau','Lamb'] # Frequency
         self.par.loc['Gau','dx']= self.par.loc['Gau','Lamb']/20 # x Delta
         self.par.loc['Gau','Fx']= 1/self.par.loc['Gau','dx']
@@ -113,7 +113,7 @@ class SimuParam():
         self.par.loc[Name,'Nt']=len(t) # x space Points (odd)
 
 
-        # self.par.loc[Name,'Lamb']=1/np.sqrt(self.emconst['e0'][0]*self.emconst['u0'][0])/self.par.loc[Name,'f']# Frequency
+        # self.par.loc[Name,'Lamb']=1/np.sqrt(self.emconst['e0']*self.emconst['u0'])/self.par.loc[Name,'f']# Frequency
         # self.par.loc[Name,'kx']=2*np.pi/self.par.loc[Name,'Lamb'] # Frequency
         # self.par.loc[Name,'dx']= self.par.loc[Name,'Lamb']/20 # x Delta
         # self.par.loc[Name,'Fx']= 1/self.par.loc[Name,'dx']
